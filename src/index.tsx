3912e819497468b53fd131999876e0fbb5b7c15a
Giorgio Mazzei <giorgio.mazzei@outlook.com>
2022-06-10 12:47:50 +0200

    feat: removed about page

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./components/pages/home/Home";
 import Details from "./components/pages/details/Details";
-import About from "./components/pages/about/About";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -15,7 +14,6 @@ root.render(
       <Routes>
         <Route index element={<Home />} />
         <Route path="details" element={<Details />} />
-        <Route path="about" element={<About />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
